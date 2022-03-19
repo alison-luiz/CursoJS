9561be5b64c08f245be9c6f8049a0c0d9ed2b654
Alison Luiz <alison_luiz@outlook.com.br>
2022-03-19 18:07:21 -0300

    Update code

--- CursoJS/aula09-var-let/index.js
+++ CursoJS/aula09-var-let/index.js
@@ -1,4 +1,14 @@
-var nome = 'Alison';
-var nome = 'Luiz'; // Usar a VAR novamente, ela redeclara seu valor da variavel
+// var nome = 'Alison';
+// var nome = 'Luiz'; // Usar a VAR novamente, ela redeclara seu valor da variavel
 
-console.log(nome);
+// console.log(nome);
+
+
+
+
+
+// TIPOS DE DADOS PRIMITIVOS
+
+const nome = 'Alison';
+const nome1 = "Alison";
+const nome2 = `Alison`;
