81c902c2ccf89159b0274e62c148247090face69
Alison Luiz <alison_luiz@outlook.com.br>
2022-03-30 23:53:56 -0300

    Update code

---
+++ CursoJS/aula14-numbers/index.js
@@ -0,0 +1,14 @@
+let num1 = 100;
+let num2 = 2.5;
+let num3 = 10.544774588741;
+
+console.log(num1 + num2);
+
+console.log(num1.toString() + num2.toString()); // Transforma o numero em uma string
+console.log(typeof num1);
+console.log(num1.toString(2)); // Representação binaria do numero
+
+console.log(num3.toFixed(2)); // Deixa somente duas casas decimais no numero
+
+console.log(Number.isInteger(num1)); // Se a variavel é um numero intenro ou não
+console.log(Number.isInteger(num2));
