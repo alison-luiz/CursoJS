b4cb65bd052ac4e0e9ad3a5730e91f6da758ec71
Alison Luiz <alison_luiz@outlook.com.br>
2022-06-14 09:10:13 -0300

    Update code

---
+++ CursoJS/aula27-operacao-ternaria/index.js
@@ -0,0 +1,13 @@
+/* 
+
+? -> Valor verdadeiro
+: -> Valor para falso
+
+(condição) ? 'Valor verdadeiro' : 'Valor falso'
+
+*/
+
+const user = 1000;
+
+const nivelUser = user >= 1000 ? 'User VIP' : 'User normal';
+console.log(nivelUser);
---
+++ CursoJS/aula28-object-date/index.js
@@ -0,0 +1,12 @@
+const data = new Date('2022-06-12 14:30:59');
+
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth() + 1);
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('ms', data.getMilliseconds());
+console.log('Dia-semana', data.getDay());
+
+console.log(data.toString());
