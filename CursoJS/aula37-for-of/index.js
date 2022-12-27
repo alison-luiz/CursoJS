bba80137f36d834f92630424bec7c5ebbf732ec4
Alison Luiz <alison_luiz@outlook.com.br>
2022-12-27 11:49:01 -0300

    Update code

--- CursoJS/aula37-for-of/index.js
+++ CursoJS/aula37-for-of/index.js
@@ -1,22 +1,34 @@
-// indices    012345678910
-const nome = ['Alison', 'Luiz'];
+const nomes = ['Alison', 'Luiz'];
 
 // Usando for comum
 // Usado com const iteráveis (arrays ou strings)
-for (let i = 0; i < nome.length; i++) {
-    console.log(nome[i]);
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i]);
 }
 
 // Usando for in
-// Usado para retornar índices ou chaves (strings, arrays ou objetos)
+// --> Usado para retornar índices ou chaves (strings, arrays ou objetos)
 // Vai retornar o indice do array, usamos a variavel original + o for in pra puxar o valor
-for (let x in nome) {
-    console.log(nome[x]);
+for (let x in nomes) {
+    console.log(nomes[x])
 }
 
 // Exemplo usando for of
-// Usado para retornar o valor em si
+// --> Usado para retornar o valor em si
 // Vai retornar o valor dos indices do array
-for (let valor of nome) {
+for (let valor of nomes) {
     console.log(valor);
+}
+
+nomes.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array);
+});
+
+const pessoa = {
+    nome: 'Alison',
+    sobrenome: 'Luiz'
+}
+
+for (let i in pessoa) {
+    console.log(i, pessoa[i])
 }
