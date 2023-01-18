a3a89549b06274f1dd9d1ac899482ee55ebb4714
Alison Luiz <alison_luiz@outlook.com.br>
2023-01-17 23:21:52 -0300

    Update code

--- Seção 5 - Arrays  (Avançado)/aula62-revisão-arrays/index.js
+++ Seção 5 - Arrays  (Avançado)/aula62-revisão-arrays/index.js
@@ -2,5 +2,5 @@
 const nomes = ['Alison', 'Luiz'];
 nomes[1] = 'Silva';
 console.log(nomes);
-delete nomes[0];
+delete nomes[0]; // Removo o elemento do array deixando um item vazio.
 console.log(nomes);
