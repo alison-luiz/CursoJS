fef967c7498ee880ecd24c34bda89e7c76564c32
Alison Luiz <alison_luiz@outlook.com.br>
2023-02-08 12:34:35 -0300

    Update code

--- Seção 5 - Arrays  (Avançado)/aula67-reduce/index.js
+++ Seção 5 - Arrays  (Avançado)/aula67-reduce/index.js
@@ -2,3 +2,24 @@
 
 // Indices       0  1  2  3  4  5  6  7  .....
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const total = numeros.reduce(function(acumulador, valor, indice, array) {
+    acumulador += valor;
+    return acumulador;
+}, 0);
+
+console.log(total);
+
+
+// Retornar a pessoa com mais idade utilizando reduce
+const pessoas = [
+    { nome: 'Alison', idade: 24},
+    { nome: 'Jhenny', idade: 25},
+    { nome: 'Maria', idade: 56},
+    { nome: 'Jose', idade: 69}
+];
+
+const maisVelha = pessoas.reduce(function(acumulador, valor) {
+    if (acumulador.idade > valor.idade) return acumulador;
+    return valor;
+});
+console.log(maisVelha);
