5a4a584b65ac729f0325888cc23468aedca3d7a3
Alison Luiz <alison_luiz@outlook.com.br>
2023-02-16 12:49:05 -0300

    Update code

--- Seção 6 - JavaScript Objetiso e Prototypes (Avançado)/aula71-Object.defineProperty()-e-Object.defineProperties()/index.js
+++ Seção 6 - JavaScript Objetiso e Prototypes (Avançado)/aula71-Object.defineProperty()-e-Object.defineProperties()/index.js
@@ -1,5 +1,5 @@
 // defineProperty e defineProperties
-function Produto(nome, preco, estoque) {
+function Produto1(nome, preco, estoque) {
     this.nome = nome;
     this.preco = preco;
     this.estoque = estoque;
@@ -8,8 +8,34 @@ function Produto(nome, preco, estoque) {
         enumerable: true, // Mostra a chave
         value: estoque, // Retorna o valor da chave
         writable: false, // Controla se o valor pode ou não ser alterado
-        configurable: false //
+        configurable: false // Configurável 
     });
 }
 
-const p1 = new Produto('Camiseta', 20, 3);
+const p1 = new Produto1('Camiseta', 20, 3);
+console.log(Object.keys(p1));
+
+
+
+function Produto2(nome, preco, estoque) {
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true, // Mostra a chave
+            value: nome, // Retorna o valor da chave
+            writable: false, // Controla se o valor pode ou não ser alterado
+            configurable: false // Configurável
+        },
+        preco: {
+            enumerable: true, // Mostra a chave
+            value: preco, // Retorna o valor da chave
+            writable: false, // Controla se o valor pode ou não ser alterado
+            configurable: false // Configurável
+        },
+        estoque: {
+            enumerable: true, // Mostra a chave
+            value: estoque, // Retorna o valor da chave
+            writable: false, // Controla se o valor pode ou não ser alterado
+            configurable: false // Configurável
+        }
+    })
+}
---
+++ Seção 6 - JavaScript Objetiso e Prototypes (Avançado)/aula72-Getters-Setters/index.js
@@ -0,0 +1,28 @@
+// defineProperty e defineProperties
+function Produto1(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+    this.estoque = estoque;
+
+    let estoquePrivado = estoque;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // Mostra a chave
+        configurable: false, // Configurável 
+
+        get: function() {
+            return estoquePrivado;
+        },
+        set: function(valor) {
+            if (typeof valor !== 'number'){
+                console.log('Valor inválido!')
+                return;
+            }
+            estoquePrivado = valor
+        }
+    });
+}
+
+const p1 = new Produto1('Camiseta', 20, 3);
+p1.estoque = 1
+console.log(p1.estoque);
